2d67afa073b0ee11788a5cc34a2ec032001e9dd6
KhetwalDevesh <deveshkhetwal05@gmail.com>
2023-11-10 12:34:58 +0530

    init and final

---
+++ main.go
@@ -0,0 +1,166 @@
+package main
+
+import (
+	"crypto/md5"
+	"crypto/sha256"
+	"encoding/hex"
+	"encoding/json"
+	"fmt"
+	"github.com/gorilla/mux"
+	"io"
+	"log"
+	"net/http"
+	"time"
+)
+
+type Block struct {
+	Pos       int
+	Data      BookCheckout
+	TimeStamp string
+	Hash      string
+	PrevHash  string
+}
+
+type BookCheckout struct {
+	BookID       string `json:"book_id"`
+	User         string `json:"user"`
+	CheckoutDate string `json:"checkout_date"`
+	IsGenesis    bool   `json:"is_genesis"`
+}
+
+type Book struct {
+	ID          string `json:"id"`
+	Title       string `json:"title"`
+	Author      string `json:"author"`
+	PublishDate string `json:"publish_date"`
+	ISBN        string `json:"isbn"`
+}
+
+type Blockchain struct {
+	blocks []*Block
+}
+
+var blockChain *Blockchain
+
+func getBlockchain(w http.ResponseWriter, r *http.Request) {
+	jbytes, err := json.MarshalIndent(blockChain.blocks, "", " ")
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		json.NewEncoder(w).Encode(err)
+		return
+	}
+	io.WriteString(w, string(jbytes))
+}
+
+func (b *Block) generateHash() {
+	bytes, _ := json.Marshal(b.Data)
+	data := string(b.Pos) + b.TimeStamp + string(bytes) + b.PrevHash
+	hash := sha256.New()
+	hash.Write([]byte(data))
+	b.Hash = hex.EncodeToString(hash.Sum(nil))
+}
+
+func CreateBlock(prevBlock *Block, checkoutItem BookCheckout) *Block {
+	fmt.Println("i am inside CreateBlock")
+	block := &Block{}
+	block.Pos = prevBlock.Pos + 1
+	block.Data = checkoutItem
+	block.TimeStamp = time.Now().String()
+	block.PrevHash = prevBlock.Hash
+	block.generateHash()
+	return block
+}
+
+func (b *Block) validateHash(hash string) bool {
+	b.generateHash()
+	if b.Hash != hash {
+		return false
+	}
+	return true
+}
+
+func validBlock(block, prevBlock *Block) bool {
+	if prevBlock.Hash != block.PrevHash {
+		return false
+	}
+	if !block.validateHash(block.Hash) {
+		return false
+	}
+	if prevBlock.Pos+1 != block.Pos {
+		return false
+	}
+	return true
+}
+
+func (bc *Blockchain) AddBlock(data BookCheckout) {
+	prevBlock := bc.blocks[len(bc.blocks)-1]
+	fmt.Println("Previous Block : ", prevBlock)
+	block := CreateBlock(prevBlock, data)
+
+	if validBlock(block, prevBlock) {
+		bc.blocks = append(bc.blocks, block)
+	}
+}
+
+func writeBlock(w http.ResponseWriter, r *http.Request) {
+	var checkoutItem BookCheckout
+	if err := json.NewDecoder(r.Body).Decode(&checkoutItem); err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		log.Printf("Could not write block : %v", err)
+		w.Write([]byte("Could not write block"))
+	}
+	blockChain.AddBlock(checkoutItem)
+	resp, _ := json.MarshalIndent(checkoutItem, "", " ")
+	w.WriteHeader(http.StatusOK)
+	w.Write(resp)
+}
+
+func newBook(w http.ResponseWriter, r *http.Request) {
+	var book Book
+	if err := json.NewDecoder(r.Body).Decode(&book); err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		log.Printf("Could not create : %v", err)
+		w.Write([]byte("Could not create new book"))
+		return
+	}
+
+	h := md5.New()
+	io.WriteString(h, book.ISBN+book.PublishDate)
+	book.ID = fmt.Sprintf("%x", h.Sum(nil))
+	resp, err := json.MarshalIndent(book, "", " ")
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		log.Printf("Could not marshall payload : %v", err)
+		w.Write([]byte("Could not save book data"))
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+	w.Write(resp)
+}
+
+func GenesisBlock() *Block {
+	return CreateBlock(&Block{}, BookCheckout{IsGenesis: true})
+}
+
+func NewBlockchain() *Blockchain {
+	return &Blockchain{[]*Block{GenesisBlock()}}
+}
+
+func main() {
+	blockChain = NewBlockchain()
+	r := mux.NewRouter()
+	r.HandleFunc("/", getBlockchain).Methods("GET")
+	r.HandleFunc("/", writeBlock).Methods("POST")
+	r.HandleFunc("/new", newBook).Methods("POST")
+	go func() {
+		for _, block := range blockChain.blocks {
+			fmt.Printf("Prev Hash : %x\n", block.PrevHash)
+			bytes, _ := json.MarshalIndent(block.Data, "", " ")
+			fmt.Printf("Data : %v\n", string(bytes))
+			fmt.Printf("Hash : %x\n", block.Hash)
+			fmt.Println()
+		}
+	}()
+	log.Println("Listening on port 3000")
+	log.Fatal(http.ListenAndServe(":3000", r))
+}
